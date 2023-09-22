16724b31a5e09aad95fd8331af3c413563f8fc1c
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-09-22 18:13:40 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <ostream>
+
+using namespace std;
+
+template <class T>
+class Container {
+
+public:
+    vector <T> data;
+    void add(T element) {
+        data.push_back(element);
+    }
+
+    void display()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cout << data[i] << endl;
+        }
+    }
+};
+
+
+
+int main() 
+{
+
+    Container<int>* container = new Container <int>();
+
+
+    container->add(5);
+    container->add(2);
+    container->add(8);
+    container->add(1);
+    container->display();
+
+}
